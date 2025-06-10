01a9ad9c372e37357915a93a5ea02bf6a35c1adf
Giulia Caglia <cagliagiulia42@gmail.com>
2025-06-10 06:40:57 +0200

    Config: Set up webpage

---
+++ timer/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<main>
+  <h1 class="title">Focus Timer</h1>
+  <div class="column">
+    <div class="timer-block">Timer Block</div>
+    <div class="video-embedding">Video Block</div>
+  </div>
+</main>
+
+<style>
+.timer-block {
+  color: blue;
+  border: 2px solid black;
+  padding-top: 150px;
+  padding-bottom: 150px;
+  padding-left: 300px;
+  padding-right: 300px;
+}
+.video-embedding {
+  color: red;
+  border: 2px solid black;
+  padding-top: 150px;
+  padding-bottom: 150px;
+  padding-left: 400px;
+  padding-right: 400px;
+}
+
+.column {
+  justify-content: flex-start;
+  display: flex;
+  gap: 20px
+}
+</style>
